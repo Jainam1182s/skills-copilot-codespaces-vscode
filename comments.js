f63ce2f827fc8f33f62c7580015c6aa689575d4f
Jainam Priteshkumar Shah <128902428+Jainam1182s@users.noreply.github.com>
2024-02-25 10:14:27 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,25 @@
+// Create web server
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
+    const path = url.parse(req.url, true).pathname;
+
+    if (path === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello, this is the homepage!\n');
+    } else if (path === '/about') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello, this is the about page!\n');
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Page not found\n');
+    }
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Server running at http://127.0.0.1:3000/');
 });
